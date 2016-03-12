fd3ffdf92c074c1771f90e4a8339d51abaefbdba
Andrew Dang-Tran <andrewdt1506@gmail.com>
2016-03-11 18:10:21 -0600

    Created Models.py which contains our 'Pillars' as data structures.

---
+++ Models.py
@@ -0,0 +1,47 @@
+class Types():
+    def __init__(self, ID, name, res, strong, im):
+        self.ID = ID
+        self.name = name
+        self.resistance = res
+        self.strength = strong
+        self.immunity = im
+
+class Pokemon():
+    def __init__(self, ID, type1, type2, heldItem, encID, move, spriteID, baseStats, evolvesInto, evolvesFrom):
+        self.ID = ID
+        self.pType1 = type1
+        self.pType2 = type2
+        self.heldItem = heldItem
+        self.encounter = encID
+        self.move = move
+        self.spriteID = spriteID
+        self.baseStats = baseStats
+        self.evolvesInto = evolvesInto
+        self.evolvesFrom = evolvesFrom
+
+class Location():
+    def __init__(self, ID, name, region)
+        self.ID = ID
+        self.name = name
+        self.region = region
+
+class Moves():
+    def __init__(self, ID, acc, pp, prio, pwr, isSpecial, t)
+        self.ID = ID
+        self.accuracy = acc
+        self.pp = pp
+        self.priority = prio
+        self.power = pwr
+        self.isSpecial = isSpecial
+        self.mType = t
+
+class Item():
+    def __init__(self, ID, name, cost, sprite, flavTxt)
+        self.ID = ID
+        self.name = name
+        self.cost = cost
+        self.sprite = sprite
+        self.flavorText = flavTxt
+
+
+
