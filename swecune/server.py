dd70cdf3c70f462e892fc2fb9f49c1f1a2d901c1
Shubham Thakkar <shubhamt@utexas.edu>
2016-03-21 17:22:49 -0500

    Added table functionality

--- swecune/server.py
+++ swecune/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
