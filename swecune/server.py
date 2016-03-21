f3271a0b79a042c2dad346892e2c2d6a0c703fe2
schun <s.chun@utexas.edu>
2016-03-21 18:30:52 -0500

    Started moves pages

--- swecune/server.py
+++ swecune/server.py
@@ -4,34 +4,46 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from flask import Flask, render_template
 import Models
 import json
+
 app = Flask(__name__)
 
 
 with open("static/pokemon/1.json") as fi:
-    bulba_data = json.load(fi)
+  bulba_data = json.load(fi)
 
 pokemon_dict = {"1": Models.Pokemon(bulba_data)}
 
+def get_move(id):
+  return {
+    "id": 1,
+    "name": "Tackle",
+    "accuracy": 100,
+    "pp": 35,
+    "priority": 0,
+    "power" : 50,
+    "is_special": False,
+    "type": 0,
+  }
+
 @app.route('/pokemon/<pokemon_number>')
 def pokemon(pokemon_number):
-    pk = pokemon_dict[pokemon_number]
-    return render_template('pokemon.html',
-            p_id=pk.ID,
-            name=pk.name,
-            p_type1=pk.pType1,
-            p_type2=pk.pType2,
-            stats=pk.baseStats)
+  pk = pokemon_dict[pokemon_number]
+  return render_template('pokemon.html',
+                          p_id=pk.ID,
+                          name=pk.name,
+                          p_type1=pk.pType1,
+                          p_type2=pk.pType2,
+                          stats=pk.baseStats)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
-
-@app.route('/table')
-def table():
-    return render_template('table.html')
-
+@app.route('/move/<move_id>')
+def move(move_id):
+  move = get_move(move_id)
+  return render_template('move.html', move=move, type_img='/static/img/fire_type.png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
