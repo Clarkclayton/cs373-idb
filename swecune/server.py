d946e777b23dbda07420a3862ec16774db1d4da0
Andrew Dang-Tran <andrewdt1506@gmail.com>
2016-03-21 19:34:30 -0500

    forgot to add server.py

--- swecune/server.py
+++ swecune/server.py
@@ -22,9 +22,17 @@ def get_move(id):
     "priority": 0,
     "power" : 50,
     "is_special": False,
-    "type": 0,
+     "type": 0,
   }
 
+def get_type(id):
+  type_dict = OrderedDict([
+    ("name", "Fire"),
+    ("numPrimary", 5),
+    ("numSecondary", 2),
+    ("Generation", 1)
+  ])
+  return type_dict
 @app.route('/pokemon/<pokemon_number>')
 def pokemon(pokemon_number):
   pk = pokemon_dict[pokemon_number]
@@ -44,6 +52,11 @@ def index():
 def move(move_id):
   move = get_move(move_id)
   return render_template('move.html', move=move, type_img='/static/img/fire_type.png')
+
+@app.route('/type/<type_id>')
+def type(type_id):
+  t = get_type(type_id)
+  return render_template('type.html', ty=t, type_img='/static/img/fire_type.png')
 
 if __name__ == '__main__':
   app.run(debug=True)
