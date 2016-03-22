31218b2b00c0f911c38b3d9fbdd94b14153d2978
schun <s.chun@utexas.edu>
2016-03-21 19:28:49 -0500

    Made progress in moves.html

--- swecune/server.py
+++ swecune/server.py
@@ -4,6 +4,7 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from flask import Flask, render_template
 import Models
 import json
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -14,16 +15,17 @@ with open("static/pokemon/1.json") as fi:
 pokemon_dict = {"1": Models.Pokemon(bulba_data)}
 
 def get_move(id):
-  return {
-    "id": 1,
-    "name": "Tackle",
-    "accuracy": 100,
-    "pp": 35,
-    "priority": 0,
-    "power" : 50,
-    "is_special": False,
-    "type": 0,
-  }
+  move_dict = OrderedDict([
+    ("ID", 1),
+    ("Name", "Tackle"),
+    ("Accuracy", 100),
+    ("PP", 35),
+    ("Priority", 0),
+    ("Power", 50),
+    ("Is Special", False),
+    ("Type", 0)
+  ])
+  return move_dict
 
 @app.route('/pokemon/<pokemon_number>')
 def pokemon(pokemon_number):
@@ -43,7 +45,9 @@ def index():
 @app.route('/move/<move_id>')
 def move(move_id):
   move = get_move(move_id)
-  return render_template('move.html', move=move, type_img='/static/img/fire_type.png')
+  pk_list = [pokemon_dict["1"]]
+
+  return render_template('move.html', move=move, type_img='/static/img/fire_type.png', pokemon_list=pk_list, pokemon_url='/static/img/pokemon1.png')
 
 if __name__ == '__main__':
   app.run(debug=True)
