5d7e4dfed3e2ccfb9f55a460606031118e73b9e4
rfaulk <bobfaulk@live.com>
2016-04-04 17:15:36 -0500

    added pagination to pokemon table

--- swecune/static/js/poke-table.js
+++ swecune/static/js/poke-table.js
@@ -2,6 +2,18 @@ var data = [
     {id: 1, name: "Bulbasaur", type_id_1: 12, type_name_1: "grass", average_stats: 53,
     type_id_2: 4, type_name_2: "poison"},
     {id: 4, name: "Charmander", type_id_1: 10, type_name_1: "fire", average_stats: 52},
+    {id: 7, name: "Squirle", type_id_1: 11, type_name_1: "water", average_stats: 52},
+    {id: 1, name: "Bulbasaur", type_id_1: 12, type_name_1: "grass", average_stats: 53,
+    type_id_2: 4, type_name_2: "poison"},
+    {id: 4, name: "Charmander", type_id_1: 10, type_name_1: "fire", average_stats: 52},
+    {id: 7, name: "Squirle", type_id_1: 11, type_name_1: "water", average_stats: 52},
+    {id: 1, name: "Bulbasaur", type_id_1: 12, type_name_1: "grass", average_stats: 53,
+    type_id_2: 4, type_name_2: "poison"},
+    {id: 4, name: "Charmander", type_id_1: 10, type_name_1: "fire", average_stats: 52},
+    {id: 7, name: "Squirle", type_id_1: 11, type_name_1: "water", average_stats: 52},
+    {id: 1, name: "Bulbasaur", type_id_1: 12, type_name_1: "grass", average_stats: 53,
+    type_id_2: 4, type_name_2: "poison"},
+    {id: 4, name: "Charmander", type_id_1: 10, type_name_1: "fire", average_stats: 52},
     {id: 7, name: "Squirle", type_id_1: 11, type_name_1: "water", average_stats: 52}
 ];
 
@@ -30,13 +42,33 @@ render: function(){
     }
 });
 
-var PokeTable = React.createClass({
-render: function(){
-    var rowNodes = this.props.data.map(function(pk){
-        return (<PokeRow pokemon={pk} key={pk.id}/>)
-    });
+var k = 0;
 
+var SLICE_WIDTH = 4;
+
+var PokeTable = React.createClass({
+getInitialState: function(){
+        return ({
+            rowNodes: this.props.data.slice(0, SLICE_WIDTH).map(function(pk){
+                k++;
+                return (<PokeRow pokemon={pk} key={k}/>)
+            })
+        })
+    },
+
+changePage: function(page){
+        console.log("page: " + page);
+        this.setState({
+            rowNodes: this.props.data.slice((page-1) * SLICE_WIDTH, page * SLICE_WIDTH).map(function(pk){
+                k++;
+                return (<PokeRow pokemon={pk} key={k}/>)
+            })
+        })
+    },
+
+render: function(){
     return(
+        <div>
         <table className="poke-table sortable-theme-bootstrap" data-sortable>
             <thead>
                 <tr>
@@ -49,9 +81,41 @@ render: function(){
                 </tr>
             </thead>
             <tbody>
-            {rowNodes}
+            {this.state.rowNodes}
             </tbody>
-        </table>)
+        </table>
+        <PokePage p={this}/>
+        </div>)
+    }
+});
+
+var handleClick = function(table, page){
+    console.log(table);
+    table.changePage(page);
+}
+
+var PokePage = React.createClass({
+render: function(){
+    return(
+        <nav>
+           <ul className="pagination">
+               <li>
+                   <a href="#" aria-label="Previous">
+                   <span aria-hidden="true">&laquo;</span>
+                   </a>
+               </li>
+               <li><a href="#" onClick={handleClick.bind(this, this.props.p, 1)}>1</a></li>
+               <li><a href="#" onClick={handleClick.bind(this, this.props.p, 2)}>2</a></li>
+               <li><a href="#" onClick={handleClick.bind(this, this.props.p, 3)}>3</a></li>
+               <li><a href="#" onClick={handleClick.bind(this, this.props.p, 4)}>4</a></li>
+               <li><a href="#" onClick={handleClick.bind(this, this.props.p, 5)}>5</a></li>
+               <li>
+                   <a href="#" aria-label="Next">
+                   <span aria-hidden="true">&raquo;</span>
+                   </a>
+               </li>
+           </ul>
+        </nav>)
     }
 });
 
