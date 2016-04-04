cc3d7360be99f7327abe1bf67504f2e555bef301
Shubham Thakkar <shubhamt@utexas.edu>
2016-04-04 03:46:57 -0500

    Added working scraper and correct models

--- models.py
+++ models.py
@@ -1,9 +1,6 @@
-from sqlalchemy import Column, Integer, ForeignKey, String
-from sqlalchemy import Table, UniqueConstraint
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, ForeignKey, String, Table, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -15,47 +12,46 @@ pokemon_move = Table('pokemon_move', Base.metadata,
                      )
 
 double_damage_to = Table('double_damage_to', Base.metadata,
-                  Column('id', Integer, primary_key=True),
-                  Column('origin', Integer, ForeignKey('type.id')),
-                  Column('opposing', Integer, ForeignKey('type.id')),
-                  UniqueConstraint('origin', 'opposing', name='double_to_relation')
-                  )
+                         Column('id', Integer, primary_key=True),
+                         Column('origin', Integer, ForeignKey('type.id')),
+                         Column('opposing', Integer, ForeignKey('type.id')),
+                         UniqueConstraint('origin', 'opposing', name='double_to_relation')
+                         )
 
 double_damage_from = Table('double_damage_from', Base.metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('origin', Integer, ForeignKey('type.id')),
-                   Column('opposing', Integer, ForeignKey('type.id')),
-                   UniqueConstraint('origin', 'opposing', name='double_from_relation')
-                   )
-
+                           Column('id', Integer, primary_key=True),
+                           Column('origin', Integer, ForeignKey('type.id')),
+                           Column('opposing', Integer, ForeignKey('type.id')),
+                           UniqueConstraint('origin', 'opposing', name='double_from_relation')
+                           )
 
 half_damage_to = Table('half_damage_to', Base.metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('origin', Integer, ForeignKey('type.id')),
-                   Column('opposing', Integer, ForeignKey('type.id')),
-                   UniqueConstraint('origin', 'opposing', name='half_to_relation')
-                   )
+                       Column('id', Integer, primary_key=True),
+                       Column('origin', Integer, ForeignKey('type.id')),
+                       Column('opposing', Integer, ForeignKey('type.id')),
+                       UniqueConstraint('origin', 'opposing', name='half_to_relation')
+                       )
 
 half_damage_from = Table('half_damage_from', Base.metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('origin', Integer, ForeignKey('type.id')),
-                   Column('opposing', Integer, ForeignKey('type.id')),
-                   UniqueConstraint('origin', 'opposing', name='half_from_relation')
-                   )
+                         Column('id', Integer, primary_key=True),
+                         Column('origin', Integer, ForeignKey('type.id')),
+                         Column('opposing', Integer, ForeignKey('type.id')),
+                         UniqueConstraint('origin', 'opposing', name='half_from_relation')
+                         )
 
 no_damage_to = Table('no_damage_to', Base.metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('origin', Integer, ForeignKey('type.id')),
-                   Column('opposing', Integer, ForeignKey('type.id')),
-                   UniqueConstraint('origin', 'opposing', name='no_to_relation')
-                   )
+                     Column('id', Integer, primary_key=True),
+                     Column('origin', Integer, ForeignKey('type.id')),
+                     Column('opposing', Integer, ForeignKey('type.id')),
+                     UniqueConstraint('origin', 'opposing', name='no_to_relation')
+                     )
 
 no_damage_from = Table('no_damage_from', Base.metadata,
-                   Column('id', Integer, primary_key=True),
-                   Column('origin', Integer, ForeignKey('type.id')),
-                   Column('opposing', Integer, ForeignKey('type.id')),
-                   UniqueConstraint('origin', 'opposing', name='no_from_relation')
-                   )
+                       Column('id', Integer, primary_key=True),
+                       Column('origin', Integer, ForeignKey('type.id')),
+                       Column('opposing', Integer, ForeignKey('type.id')),
+                       UniqueConstraint('origin', 'opposing', name='no_from_relation')
+                       )
 
 
 class Pokemon(Base):
@@ -78,6 +74,17 @@ class Pokemon(Base):
 
     moves = relationship('Move', secondary=pokemon_move, back_populates='pokemon')
 
+    def __init__(self, id, name, hp, attack, defense, special_attack, special_defense, speed, average_stats):
+        self.id = id
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.special_attack = special_attack
+        self.special_defense = special_defense
+        self.speed = speed
+        self.average_stats = average_stats
+
 
 class Move(Base):
     __tablename__ = 'move'
@@ -94,6 +101,15 @@ class Move(Base):
 
     pokemon = relationship('Pokemon', secondary=pokemon_move, back_populates='moves')
 
+    def __init__(self, id, name, accuracy, pp, priority, power, damage_class):
+        self.id = id,
+        self.name = name,
+        self.accuracy = accuracy,
+        self.pp = pp,
+        self.priority = priority,
+        self.power = power,
+        self.damage_class = damage_class
+
 
 class Type(Base):
     __tablename__ = 'type'
@@ -104,116 +120,42 @@ class Type(Base):
     move_type = relationship("Move", back_populates="type")
 
     double_damage_to = relationship('Type',
-                      secondary=double_damage_to,
-                      primaryjoin=id==double_damage_to.c.origin,
-                      secondaryjoin=id==double_damage_to.c.opposing,
-                      backref='double_damage_to_backref')
-    double_damage_from = relationship('Type', secondary=double_damage_from, primaryjoin=id==double_damage_from.c.origin, secondaryjoin=id==double_damage_from.c.opposing, backref='double_damage_from_backref')
+                                    secondary=double_damage_to,
+                                    primaryjoin=id == double_damage_to.c.origin,
+                                    secondaryjoin=id == double_damage_to.c.opposing,
+                                    backref='double_damage_to_backref')
 
-    half_damage_to = relationship('Type', secondary=half_damage_to, primaryjoin=id==half_damage_to.c.origin, secondaryjoin=id==half_damage_to.c.opposing, backref='half_damage_to_backref')
-    half_damage_from = relationship('Type', secondary=half_damage_from, primaryjoin=id==half_damage_from.c.origin, secondaryjoin=id==half_damage_from.c.opposing, backref='half_damage_from_backref')
+    double_damage_from = relationship('Type',
+                                      secondary=double_damage_from,
+                                      primaryjoin=id == double_damage_from.c.origin,
+                                      secondaryjoin=id == double_damage_from.c.opposing,
+                                      backref='double_damage_from_backref')
 
-    no_damage_to = relationship('Type', secondary=no_damage_to, primaryjoin=id==no_damage_to.c.origin, secondaryjoin=id==no_damage_to.c.opposing, backref='no_damage_to_backref')
-    no_damage_from = relationship('Type', secondary=no_damage_from, primaryjoin=id==no_damage_from.c.opposing, secondaryjoin=id==no_damage_from.c.opposing, backref='no_damage_from_backref')
+    half_damage_to = relationship('Type',
+                                  secondary=half_damage_to,
+                                  primaryjoin=id == half_damage_to.c.origin,
+                                  secondaryjoin=id == half_damage_to.c.opposing,
+                                  backref='half_damage_to_backref')
 
+    half_damage_from = relationship('Type',
+                                    secondary=half_damage_from,
+                                    primaryjoin=id == half_damage_from.c.origin,
+                                    secondaryjoin=id == half_damage_from.c.opposing,
+                                    backref='half_damage_from_backref')
 
+    no_damage_to = relationship('Type',
+                                secondary=no_damage_to,
+                                primaryjoin=id == no_damage_to.c.origin,
+                                secondaryjoin=id == no_damage_to.c.opposing,
+                                backref='no_damage_to_backref')
 
-dialect = 'mysql+pymysql'
-username = 'root'
-password = '77corvette'
-host = '127.0.0.1'
-port = '3306'
-database = 'SWECUNEDB'
+    no_damage_from = relationship('Type',
+                                  secondary=no_damage_from,
+                                  primaryjoin=id == no_damage_from.c.origin,
+                                  secondaryjoin=id == no_damage_from.c.opposing,
+                                  backref='no_damage_from_backref')
 
-engine = create_engine(
-    "{}://{}:{}@{}:{}/{}".format(dialect, username, password, host, port, database)).connect()
-
-
-# engine.execute('DROP TABLE IF EXISTS pokemon_move;')
-
-# engine.execute('DROP TABLE IF EXISTS double_damage_to;')
-# engine.execute('DROP TABLE IF EXISTS double_damage_from;')
-# engine.execute('DROP TABLE IF EXISTS half_damage_to;')
-
-# engine.execute('DROP TABLE IF EXISTS half_damage_from;')
-# engine.execute('DROP TABLE IF EXISTS no_damage_to;')
-# engine.execute('DROP TABLE IF EXISTS no_damage_from;')
-
-# engine.execute('DROP TABLE IF EXISTS pokemon;')
-# engine.execute('DROP TABLE IF EXISTS move;')
-# engine.execute('DROP TABLE IF EXISTS type;')
-
-
-# Base.metadata.create_all(engine)
-
-# m = Move(name = 'test_move', accuracy = 1, pp = 2, prioirty = 3, power=4, damange_class = 'normal')
-t1 = Type(name = 'name', generation=1)
-t2= Type(name = 'name2', generation=1)
-t3 = Type(name='name3', generation=2)
-
-# t1.strengths.append(t2)
-# t2.strengths.append(t1)
-
-p = Pokemon(name='test1')
-# m = Move(name='test2', damage_class='damage')
-# p.moves.append(m)
-p.primary_type = t3
-p.secondary_type = t3
-
-
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# session.add(p)
-session.commit()
-
-
-
-# Base.metadata.create_all(connection)
-
-# result = connection.execute('SELECT * FROM information_schema.tables;')
-# print('\n\n')
-# for row in result:
-#     print(row)
-# result = connection.execute('DESCRIBE pokemon;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE move;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE type;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE pokemon_move;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE strengths;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE weaknesses;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
-
-# result = connection.execute('DESCRIBE immunities;')
-
-# print('\n\n')
-# for row in result:
-#     print(row)
+    def __init__(self, id, name, generation):
+        self.id = id,
+        self.name = name,
+        self.generation = generation
---
+++ scraper/scrapeDB.py
@@ -0,0 +1,130 @@
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from db.Factory import Factory, pokemon, types, moves
+from db.models import Base
+
+# dialect = 'mysql+pymysql'
+# username = 'root'
+# password = '77corvette'
+# host = '127.0.0.1'
+# port = '3306'
+# database = 'SWECUNEDB'
+
+dialect = 'mysql+pymysql'
+username = 'guestbook-user'
+password = 'guestbook-user-password'
+host = '104.130.22.72'
+port = '3306'
+database = 'guestbook'
+
+engine = create_engine('{}://{}:{}@{}:{}/{}'.format(dialect, username, password, host, port, database)).connect()
+
+engine.execute('DROP TABLE IF EXISTS pokemon_move;')
+
+engine.execute('DROP TABLE IF EXISTS double_damage_to;')
+engine.execute('DROP TABLE IF EXISTS double_damage_from;')
+engine.execute('DROP TABLE IF EXISTS half_damage_to;')
+
+engine.execute('DROP TABLE IF EXISTS half_damage_from;')
+engine.execute('DROP TABLE IF EXISTS no_damage_to;')
+engine.execute('DROP TABLE IF EXISTS no_damage_from;')
+
+engine.execute('DROP TABLE IF EXISTS pokemon;')
+engine.execute('DROP TABLE IF EXISTS move;')
+engine.execute('DROP TABLE IF EXISTS type;')
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine, autocommit=True)
+session = Session()
+
+endpoints = ['type', 'pokemon', 'move']
+# endpoints = ['type']
+base_url = "http://pokeapi.co/api/v2/"
+
+for url_end in endpoints:
+    resp = requests.get(base_url + url_end).json()
+
+    urls = []
+    while len(urls) != resp['count']:
+        urls += [result['url'] for result in resp['results']]
+        if resp['next'] is not None:
+            resp = requests.get(resp['next']).json()
+
+    x = getattr(Factory, 'make_' + url_end + '_json')
+    for url in urls:
+        print(url)
+        x(requests.get(url).json())
+
+Factory.add_relationships()
+
+print('Add_all started')
+session.add_all(value for value in types.values())
+session.flush()
+print('done')
+
+print('Add_all started')
+session.add_all(value for value in moves.values())
+session.flush()
+print('done')
+
+print('Add_all started')
+session.add_all(value for value in pokemon.values())
+session.flush()
+print('done')
+
+session.close()
+
+result = engine.execute('SELECT * FROM double_damage_to')
+for row in result:
+    print(row)
+
+print('\n\n')
+
+result = engine.execute('SELECT * FROM double_damage_from')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM half_damage_to')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM half_damage_from')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM no_damage_to')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM no_damage_from')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM pokemon_move')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM type')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM move')
+for row in result:
+    print(row)
+
+print('\n\n')
+result = engine.execute('SELECT * FROM pokemon')
+for row in result:
+    print(row)
+
+engine.close()
