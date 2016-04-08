c60699c47ae65f20771752f7067a0401dcc20341
Andrew Dang-Tran <andrewdt1506@gmail.com>
2016-04-07 21:31:45 -0500

    added the button for run tests

--- swecune/static/js/runTests.js
+++ swecune/static/js/runTests.js
@@ -1,6 +1,6 @@
 document.getElementById("test-button").onclick = function () {
     var req = new XMLHttpRequest();
-    var url = "http://swecune.com/api/run_tests";
+    var url = "run_tests";
     req.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 alert(req.responseText);
