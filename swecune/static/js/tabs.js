9d0f807dc89cb8e9b54ebe99144b9ffd6552d259
Andrew Dang-Tran <andrewdt1506@gmail.com>
2016-04-15 01:08:45 -0500

    Changed search in navbar to be on one row for mobile

--- swecune/static/js/tabs.js
+++ swecune/static/js/tabs.js
@@ -1,10 +1,8 @@
 jQuery(document).ready(function() {
     jQuery('.tabs .tab-links a').on('click', function(e)  {
-        console.log("I AM RUNNING asdfasdfasdfsadfasdfasdfasdfsadfsadf");
         var currentAttrValue = jQuery(this).attr('href');
         // Show/Hide Tabs
         jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
-        console.log(currentAttrValue);
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
  
